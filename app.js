9a9ba7c62c32eac125868f6299025576c41adf01
DevMaicon7 <maicin1douglas@gmail.com>
2024-09-07 21:53:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+if (campoPesquisa == "") {
+return
+}
+campoPesquisa = campoPesquisa.toLowerCase() 
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    let imagem = "";
+    for (let dado of dados) {
+        titulo = dado.Titulo.toLocaleLowerCase()
+        descricao = dado.Descricao.toLocaleLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+            <img src="${dado.imagem}" alt="imagem do resultado" class="imagem-resultado">
+                     <h2>${dado.Titulo}</h2>
+                     <p class="descricao-meta">${dado.Descricao}</p>
+                     <a href="${dado.Link}" target="_blank">Aprenda no chess.com</a>
+                    </div>
+            `;} 
+    }
+    section.innerHTML = resultados  
+}
---
+++ dados.js
@@ -0,0 +1,149 @@
+let dados = [
+    {
+        Titulo : "Abertura Fianqueto do Rei",
+        imagem : "FianquetoDoRei.jpg",
+        Descricao : "A Abertura Fianqueto do Rei é caracterizada pelo movimento inicial das brancas: 1. g3. Nessa abertura, o bispo do rei é fianquetado na casa g2, criando uma diagonal longa para exercer pressão.",
+        Link : "https://www.chess.com/openings/Kings-Fianchetto-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Abertura do Peão da Dama",
+        imagem : "AberturaPeaoDama.jpg",
+        Descricao : "A Abertura do Peão da Dama é o conjunto de movimentos iniciais das brancas. Ela envolve o avanço do peão da dama em d2 duas casas à frente, controlando o centro do tabuleiro.",
+        Link : "https://www.chess.com/openings/Queens-Pawn-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Abertura do Peão do Rei",
+        imagem : "AberturaPeaoRei.jpg",
+        Descricao : "A Abertura do Peão do Rei é uma das opções mais populares e agressivas para as brancas começarem uma partida de xadrez. Ela se inicia com o movimento 1. e4, permitindo que as brancas assumam o controle do centro e abram linhas de desenvolvimento para suas peças.",
+        Link : "https://www.chess.com/openings/Kings-Pawn-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Ataque Ninzowitsch-Larsen",
+        imagem : "AtaqueLarsen.jpg",
+        Descricao : "O Ataque Ninzowitsch-Larsen é uma abertura de xadrez que surge após as jogadas 1. e4 e5 2. Cf3 Cc6 3. Ac4 Ac5, caracterizada por uma estrutura de peões central simétrica e um rápido desenvolvimento das peças.",
+        Link : "https://www.chess.com/openings/Nimzowitsch-Larsen-Attack",
+        tags: "ataques xadrez chess"
+    },
+    {
+        Titulo : "Abertura inglesa",
+        imagem : "AberturaInglesa.jpg",
+        Descricao : "A Abertura Inglesa é uma das aberturas de xadrez mais populares e versáteis, caracterizada pelo lance inicial 1.c4. Essa abertura oferece ao jogador de brancas uma ampla gama de possibilidades estratégicas e posicionais.",
+        Link : "https://www.chess.com/openings/English-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Abertura Bird",
+        imagem : "AberturaBird.jpg",
+        Descricao : "A Abertura Bird é uma abertura de xadrez que se caracteriza pelo lance inicial 1. f4. Essa jogada, um pouco menos comum que os tradicionais 1. e4 ou 1. d4, busca desde o início controlar o centro do tabuleiro de uma forma diferente, oferecendo um jogo posicional e estratégico.",
+        Link : "https://www.chess.com/openings/Birds-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Abertura Réti",
+        imagem : "AberturaReti.jpg",
+        Descricao : "A Abertura Réti, também conhecida como Abertura Zuckertort-Réti, é uma abertura hipermoderna de xadrez caracterizada pelo lance inicial 1.Cf3. Essa abertura, em vez de ocupar o centro do tabuleiro imediatamente, busca controlá-lo de forma indireta, pressionando-o com peças menores e peões de flanco.",
+        Link : "https://www.chess.com/openings/Reti-Opening",
+        tags: "aberturas xadrez chess"
+    },
+    {
+        Titulo : "Defesa Holandesa",
+        imagem : "DefesaHolandesa.jpg",
+        Descricao : "A Defesa Holandesa é uma abertura de xadrez que se caracteriza pelo lance inicial das pretas 1...f5. Essa jogada, um tanto incomum, busca desafiar o centro branco desde o início, oferecendo um jogo dinâmico e tático.",
+        Link : "https://www.chess.com/openings/Dutch-Defense",
+        tags: "defesas xadrez chess"
+    },
+    {
+        Titulo : "Defesa Moderna com D4",
+        imagem : "DefesaModerna.jpg",
+        Descricao : "A Defesa Moderna é uma abertura de xadrez que se caracteriza pela resposta das pretas 1...g6 ao lance inicial das brancas 1.d4. Essa abertura oferece às pretas uma estrutura sólida e flexível, permitindo um jogo posicional e estratégico.",
+        Link : "https://www.chess.com/openings/Modern-Defense-with-1-d4",
+        tags: "defesas xadrez chess"
+    },
+    {
+        Titulo : "Defesa Pirc",
+        imagem : "DefesaPirc.jpg",
+        Descricao : "A Defesa Pirc é uma abertura de xadrez que se caracteriza pela resposta das Pretas 1...d6 e 2...Cf6 ao lance inicial das Brancas 1.e4. Essa abertura oferece às Pretas uma estrutura sólida e flexível, permitindo um jogo posicional e estratégico.",
+        Link : "https://www.chess.com/openings/Pirc-Defense",
+        tags: "defesas xadrez chess"
+    },
+    {
+        Titulo : "Defesa Caro-Kann",
+        imagem : "DefesaCaroKann.jpg",
+        Descricao : "A Defesa Caro-Kann é uma abertura de xadrez sólida e confiável, caracterizada pelos lances iniciais 1.e4 c6. Essa abertura oferece às pretas uma estrutura de peões forte e compacta, permitindo um jogo posicional e estratégico.",
+        Link : "https://www.chess.com/openings/Caro-Kann-Defense",
+        tags: "defesas xadrez chess"
+    },
+    {
+        Titulo : "Mate #10: Donisthorpe vs. Mundell, 1892",
+        imagem : "Mate10.jpg",
+        Descricao : "O agradável xeque-mate de Donisthorpe não é o mais complexo que veremos, mas sua maravilha reside na natureza quase impossível do primeiro lance, bem como nos magníficos mates a que ele leva.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #9: Neugebauer vs. Degenkolbe, 1956",
+        imagem : "Mate9.jpg",
+        Descricao : "Neugebauer deve ter surpreendido Degenkolbe quando, de repente, deu a volta por cima em uma posição que parecia perdida. Essa ideia foi vista em algumas partidas, mas este é o exemplo mais antigo que vi. Se você conhece algum exemplo anterior, compartilhe-o nos comentários!",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #8: Milman vs. Fang, 2005",
+        imagem : "Mate8.jpg",
+        Descricao : "O IM Lev Milman encontra uma sequência impressionante que leva a um xeque-mate que, a princípio, é difícil de acreditar. É possível que o rei negro realmente não tenha escapatória nesta posição?",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #7: Bauer vs. Gollner, 1956",
+        imagem : "Mate7.jpg",
+        Descricao : "Esta obra-prima de Bauer foi publicada em inúmeras coleções de mate e, por um bom motivo, é uma combinação impecável, com várias linhas estéticas que levam ao xeque-mate.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #6: Kasparian vs. Manvelian, 1939",
+        imagem : "Mate6.jpg",
+        Descricao : "Embora Genrikh Kasparian não tenha alcançado o auge do mundo do xadrez competitivo, suas composições estão entre as melhores da história. Nesta partida, Kasparian deslumbra com uma combinação violenta que termina com um lance devastadoramente silencioso.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #5: Vaccaroni vs. Mazocchi, 1891",
+        imagem : "Mate5.jpg",
+        Descricao : "Embora possa ser encontrada em um problema, a combinação de Vaccaroni (mais antiga que a de Donisthorpe por apenas um ano) parece quase impossível de encontrar em uma partida real. É como um raio atingindo um céu azul claro, transformando o aparente caos em claro xeque-mate.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #4: Popov vs. Novopashin, 1979",
+        imagem : "Mate4.jpg",
+        Descricao : "Essa combinação inspirou o GM Magnus Carlsen na última partida de desempate do Campeonato Mundial de Xadrez de 2016. O que mais pode ser dito sobre isso?",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #3: Engels vs. Cardoso",
+        imagem : "Mate3.jpg",
+        Descricao : "O mate de Engels merece um lugar no top 3 graças à incrível variedade de linhas e possíveis mates. É incrível como surgem tantos xeques-mate diferentes da combinação e TODOS eles são lindos, únicos e forçados.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #2: Jung vs. Szabados, 1952",
+        imagem : "Mate2.jpg",
+        Descricao : "Sutileza é a chave para este xeque-mate. Jung cria um verdadeiro zugzwang, apenas com um bispo lutando contra a dama. Se as Negras não precisassem fazer lance, não haveria como vencer a partida. Mas elas devem fazer um lance. Mesmo com uma enorme vantagem material, cada lance leva à catástrofe.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+    {
+        Titulo : "Mate #1: Rossolimo vs. NN, 1944",
+        imagem : "Mate1.jpg",
+        Descricao : "Rossolimo executa uma série impressionante de sacrifícios para arrastar o rei negro para uma rede de mate, culminando no ataque do próprio rei branco junto com um lance de torre silencioso.",
+        Link : "https://www.chess.com/pt-BR/article/view/10-xeques-mate-mais-bonitos",
+        tags: "mates xadrez chess"
+    },
+];
